bc9957a7ca75801c0c8725eb9d7b463ac831cce4
LookRain <brantscube@hotmail.com>
2019-04-11 15:56:29 +0800

    add

--- fetch.js
+++ fetch.js
@@ -1,2 +1,10 @@
 const axios = window.axios;
-console.log(axios.get('https://tracker.kyber.network/api/tokens/pairs'));
+
+const fetchPairs = async () => {
+  const res = axios.get('https://tracker.kyber.network/api/tokens/pairs');
+  const result = res.data['ETH_TUSD'];
+  const lastPrice = 1/result.lastPrice;
+  const node = document.querySelector('.result');
+  node.innerHTML = lastPrice;
+}
+fetchPairs();
